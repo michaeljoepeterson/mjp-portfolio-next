97398793520bdd3385676d6ec697fea16631e0ce
Michael Peterson <michaeljoepeterson@gmail.com>
2023-04-02 12:25:24 -0600

    added tailwind

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,11 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+        <nav>
+          <p className="text-3xl font-bold underline">
+            Nav here with tailwind
+          </p>
+        </nav>
         <Main />
         <NextScript />
       </body>
